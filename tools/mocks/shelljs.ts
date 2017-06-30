e3292d75955b2db87554d36875a7156de961d1c8
Trotyl <trotyl@qq.com>
2017-06-30 23:03:58 +0800

    add test infrastructure

---
+++ tools/mocks/shelljs.ts
@@ -0,0 +1,5 @@
+export const cd: Function = () => {}
+export const echo: Function = () => {}
+export const exec: Function = () => {}
+export const mkdir: Function = () => {}
+export const rm: Function = () => {}
