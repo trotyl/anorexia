b8bebaea64ca0a93c3f598076aee58c26cb007e8
Trotyl <trotyl@qq.com>
2017-07-01 15:10:42 +0800

    add test for extensions init

--- test/core.spec.ts
+++ test/core.spec.ts
@@ -1,5 +1,12 @@
 import { expect } from 'chai'
-import { create, Environment } from '../src/core'
+import { stub } from 'sinon'
+import { create, Environment, ExtensionFactory } from '../src/core'
+
+declare module '../src/core' {
+  interface Extensions {
+    mock: any
+  }
+}
 
 describe('core', () => {
   it('should be able to create new environment', () => {
@@ -9,6 +16,13 @@ describe('core', () => {
   })
 
   it('should init extensions', () => {
-    
+    const MOCK_EXTENSION = 'MOCK_EXTENSION'
+    const mockExtensionFactory = stub().callsFake((env: Environment) => env.extensions.mock = MOCK_EXTENSION)
+
+    Environment.extensionFactories.push(mockExtensionFactory)
+    const env = create(__dirname)
+
+    expect(mockExtensionFactory.calledWith(env))
+    expect(env.extensions.mock).to.equal(MOCK_EXTENSION)
   })
 })
