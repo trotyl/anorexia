5a363e06b95c4dcd73d46d3bc3062db678e44ead
Trotyl <trotyl@qq.com>
2017-07-15 17:17:03 +0800

    move playbook to separate folder

--- src/anorexia.ts
+++ src/anorexia.ts
@@ -2,6 +2,7 @@ export { createHost, Host } from './core'
 export { playbook, stage } from './playbook'
 
 import './extensions/angular'
+import './extensions/file'
 import './extensions/javascript'
 import './extensions/system'
 import './extensions/text'
